8f784af5ce1416cd78e7f6252b7de3bcac7bc8e8
Yousef Amr <gooamr88@gmail.com>
2026-03-07 23:29:20 +0200

    feat: add dynamic SEO and Open Graph image

--- server/server.js
+++ server/server.js
@@ -81,7 +81,8 @@ app.use(helmet({
 const allowedOrigins = [
     'http://localhost:3000', // عشان لو بتجرب على جهازك
     'http://localhost:5000',
-'roya-advertising.com' // اللينك بتاع الإنتاج (Production)
+    // اللينك بتاع الإنتاج (Production)
+    'https://roya-advertising.com'
 ];
 
 // لو عندك لينك تاني في الـ env ضيفه برضه
